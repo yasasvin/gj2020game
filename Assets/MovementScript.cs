fab4635d79c3d90f4ae49386264a3d38349f8319
Chamara <yasasvin222@gmail.com>
2020-01-31 22:49:42 +1300

    Player movements and collision
    game mechanics

---
+++ Assets/MovementScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MovementScript : MonoBehaviour
+{
+    NavMeshAgent agent;
+
+    public LayerMask clickArea;
+    private NavMeshAgent MyAgent;
+
+    void Start()
+    {
+        MyAgent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(1))
+        {
+            Ray myRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitinfo;
+
+            if (Physics.Raycast(myRay,out hitinfo,100,clickArea))
+            {
+                MyAgent.SetDestination(hitinfo.point);
+            }
+        }
+    }
+}
