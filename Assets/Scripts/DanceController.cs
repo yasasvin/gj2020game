4dc9f0fed869ba54fd47db0dad5a5567eb239e66
Chamara <yasasvin222@gmail.com>
2020-02-02 13:15:49 +1300

    Dancing people

---
+++ Assets/Scripts/DanceController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DanceController : MonoBehaviour
+{
+    bool dancechange = (Random.value > 0.5);
+    Animator Anim;
+
+    void Start()
+    {
+        Anim = GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        dancechange = (Random.value > 0.5);
+        Anim.SetBool("ChangeMove", dancechange);
+        Debug.Log(dancechange);
+    }
+}
